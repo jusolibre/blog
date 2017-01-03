9b22d70b6c010805d73112e1dc86062ae87de05e
VincentGerard <vincent.gerard70@gmail.com>
2017-01-03 09:33:19 +0100

    maj

--- test.php
+++ test.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>form</title>
+	<title>Blog</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-
+	<link rel="stylesheet" href="html/style.css">
 </head>
 <body>
 
 
-	<form classs="form-group" action="test.php" method="post" name="formulaire">
+	<form id="form" classs="form-group" action="test.php" method="post" name="formulaire">
+		<h1>Blog</h1>
 		Auteur: <input class="form-control" type="text" name="auteur" /><br/>
 		Titre: <input class="form-control" type="text" name="titre" /><br/>
 		contenu: <textarea type="text" class="form-control" name="contenu" rows="3"></textarea><br/>
@@ -19,52 +20,46 @@
 
 	<?php
 
-	$auteur = (isset($auteur)) ? $_POST['auteur'] : null;
-	$titre = (isset($titre)) ? $_POST['titre'] : null;
-	$contenu = (isset($contenu)) ? $_POST['contenu'] : null;
-
-	if(isset($auteur) && isset($titre) && isset($contenu)){
-		echo $auteur." ".$titre." ".$contenu;}
-
-
-
-		try
-		{
+	try
+	{
 	// On se connecte à MySql
-			$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-		}
-		catch (Exception $e)
-		{
+		$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
 	// En cas d'erreur, on affiche un contenu et on arrête tout
-			die('Erreur: Le contenu est pas envoyé');
-		}
+		die('Erreur: Le contenu est pas envoyé');
+	}
 
-		if (isset($_POST['auteur'])) {    
-			$auteur = htmlspecialchars($_POST['auteur']);
-		}
-
-
-		if (isset($_POST['titre'])) {
-			$titre = htmlspecialchars($_POST['titre']);
-		}
-		if (isset($_POST['contenu'])) {
-			$contenu = htmlspecialchars($_POST['contenu']);
-		}
 // On ajoute une entrée dans la table avec une requête préparé et en utilisant des marqueurs auteurinatifs.
+	if(isset($_REQUEST['auteur']) && isset($_REQUEST['titre'])&& isset($_REQUEST['contenu'])){
+		
 
-		$req = $bdd->prepare('INSERT INTO article(auteur, titre, contenu, date_parution) VALUES(:auteur, :titre, :contenu, NOW() )');
+		$auteur = $_REQUEST['auteur'];
+		$titre = $_REQUEST['titre'];
+		$contenu= $_REQUEST['contenu'];
 
-		$err = $req->execute(array(
-			"auteur" => $auteur,
-			"titre" => $titre,
-			"contenu" => $contenu,
-			));
 
-		$req->closeCursor();
+
+		$req = $bdd->prepare('INSERT INTO article(auteur, titre, contenu, date_parution) VALUES(:auteur, :titre, :contenu, NOW())');
+
+		$req->bindParam(':auteur', $auteur);
+		$req->bindParam(':titre', $titre);
+		$req->bindParam(':contenu', $contenu);
+		$req->execute();
+		
+		echo "votre formulaire à bien été envoyer";
+		
+		/*sleep(3);*/
+
+		header('Location: index.php');
+		exit();
+
+	}
 
 	?>
 
-	</body>
-	</html>
+</body>
+</html>
 
 
